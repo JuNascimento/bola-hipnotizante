449bc9ae4bca30e72f20184454241b36b21335d8
Julia de Araújo Nascimento <ju.araujo11@yahoo.com.br>
2022-05-27 13:04:11 -0300

    Atualiza sketch

--- sketch.js
+++ sketch.js
@@ -1,29 +1,29 @@
-var bola
+let bola
 
-function setup(){
-    createCanvas(500,500);
+function setup() {
+  createCanvas(500, 500)
 
-    bola = createSprite(250,250,10,10);
-    bola.shapeColor = "red";
+  bola = createSprite(250, 250, 10, 10)
+  bola.shapeColor = 'red'
 }
 
-function draw(){
-    background("white");
+function draw() {
+  background('white')
 
-    if(keyDown(LEFT_ARROW)){
-        alteraPosicao(-1,0);
-    } else if(keyDown(RIGHT_ARROW)){
-        alteraPosicao(1,0);
-    } else if(keyDown(UP_ARROW)){
-        alteraPosicao(0,-1);
-    } else if(keyDown(DOWN_ARROW)){
-        alteraPosicao(0,+1);
-    }
+  if (keyDown(LEFT_ARROW)) {
+      alteraPosicao(-1, 0)
+  } else if (keyDown(RIGHT_ARROW)) {
+      alteraPosicao(1, 0)
+  } else if (keyDown(UP_ARROW)) {
+      alteraPosicao(0, -1)
+  } else if (keyDown(DOWN_ARROW)) {
+      alteraPosicao(0, 1)
+  }
 
-    drawSprites();
+  drawSprites()
 }
 
-function alteraPosicao(x,y){
-    bola.x = bola.x + x;
-    bola.y = bola.y + y;
+function alteraPosicao(x, y) {
+  bola.x = bola.x + x
+  bola.y = bola.y + y
 }
